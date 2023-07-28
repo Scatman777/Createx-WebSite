37ab0508c322d009ae63fed53f5d420a5a1b31f3
Scatman777 <divakovbogdan11@gmail.com>
2023-07-29 02:36:45 +0300

    Upadate

---
+++ js/Createx.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+  $('.body-main__slides').slick({
+    dots: true,
+    infinite: true,
+    speed: 1300,
+    slidesToShow: 1,
+    variableWidth: true,
+    easing: 'ease',
+    autoplay: true,
+    autoplaySpeed: 1500,
+    pauseOnHover: false,
+    draggable: false,
+    waitForAnimate: false
+  });
+});
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    } else {
+      change.target.classList.remove('element-show')
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5]
+};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
+
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function() {
+  this.classList.toggle("buy-border");
+});
+
+var btnsub = document.getElementById("button-style");
+btnsub.addEventListener("click", function() {
+  this.classList.toggle("button-background");
+});
