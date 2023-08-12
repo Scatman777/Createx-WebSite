ecc6a56614d198e8fd23a2f4d4afb99ec71166d6
Scatman777 <divakovbogdan11@gmail.com>
2023-08-12 20:35:40 +0300

    Update

--- js/Createx.js
+++ js/Createx.js
@@ -60,12 +60,13 @@ for (let elm of elements) {
 ///////////////// PASSWORD-OPEN /////////////////
 function show_hide_password(target){
 	var input = document.getElementById('password-input');
+
 	if (input.getAttribute('type') == 'password') {
 		target.classList.add('view');
 		input.setAttribute('type', 'text');
 	} else {
 		target.classList.remove('view');
-		input.setAttribute('type', 'password');
+		input.setAttribute('type', 'text');
 	}
 	return false;
 }
