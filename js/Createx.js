329e9488cfc2a952a62166771a2f93d137921a53
Scatman777 <divakovbogdan11@gmail.com>
2023-08-13 18:38:27 +0300

    Update

--- js/Createx.js
+++ js/Createx.js
@@ -72,35 +72,28 @@ function show_hide_password(target){
 }
 
 let isMobile = {
-	Android: function () { return navigator.userAgent.match(/Android/i); },
-	BlackBerry: function () { return navigator.userAgent.match(/BlackBerry/i); },
-	iOS: function () { return navigator.userAgent.match(/iPhone|iPad|iPod/i); },
-	Opera: function () { return navigator.userAgent.match(/Opera Mini/i); },
-	Windows: function () { return navigator.userAgent.match(/IEMobile/i); },
-	any: function () { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); }
+	Android: function() {return navigator.userAgent.match(/Android/i);},
+	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
 };
+		let body=document.querySelector('body');
+if(isMobile.any()){
+		body.classList.add('touch');
+		let arrow=document.querySelectorAll('.arrow');
+	for(i=0; i<arrow.length; i++){
+			let thisLink=arrow[i].previousElementSibling;
+			let subMenu=arrow[i].nextElementSibling;
+			let thisArrow=arrow[i];
 
-let body = document.querySelector('body');
-if (isMobile.any()) {
-
-	body.classList.add('touch');
-	let arrow = document.querySelectorAll('.body-up__lang-menu');
-
-	for (i = 0; i < arrow.length; i++) {
-
-		let thisLink = arrow[i].previousElementSibling;
-		let subMenu = arrow[i].nextElementSibling;
-		let thisArrow = arrow[i];
-
-		thisLink.classList.add('parent');
-		arrow[i].addEventListener('click', function () {
-
+			thisLink.classList.add('parent');
+		arrow[i].addEventListener('click', function(){
 			subMenu.classList.toggle('open');
 			thisArrow.classList.toggle('active');
-
 		});
 	}
-
-} else {
+}else{
 	body.classList.add('mouse');
 }
