6f10a5ca16ceab55bae433e5df0d4ca2d2b25553
hostr777 <divakovbogdan11@gmail.com>
2023-10-08 22:36:51 +0300

    Update

--- js/Createx.js
+++ js/Createx.js
@@ -20,14 +20,13 @@ for (let elm of elements) {
 
 
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("list").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.block-sorting__plus')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var dropdowns = document.getElementsByClassName("block-sorting__list");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
