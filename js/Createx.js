a9e5c3d48f15848ad88498849f76f6eba872ad01
hostr777 <divakovbogdan11@gmail.com>
2023-10-18 11:33:43 +0300

    Update

--- js/Createx.js
+++ js/Createx.js
@@ -18,6 +18,23 @@ for (let elm of elements) {
   observer.observe(elm);
 }
 
+const swiper = new Swiper('.swiper', {
+	direction: 'vertical',
+	loop: true,
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+	
+	scrollbar: {
+	  el: '.swiper-scrollbar',
+	},
+  });
+
 ///////////////// POPAP /////////////////
 function passwordHide() {
     document.getElementById("open").classList.toggle("password__open");
