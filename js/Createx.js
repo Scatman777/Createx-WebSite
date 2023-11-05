764d6724ee677f2c851a9e5c293e98bc978b329b
hostr777 <divakovbogdan11@gmail.com>
2023-11-05 14:16:50 +0300

    Update

--- js/Createx.js
+++ js/Createx.js
@@ -18,7 +18,7 @@ for (let elm of elements) {
   observer.observe(elm);
 }
 
-///////////////// POPAP /////////////////
+///////////////// PASSWORD-OPEN /////////////////
 document.getElementById("open-password").addEventListener("change", function() {
 
 	document.getElementById("popap-icon").classList.toggle("password__open");
@@ -27,6 +27,53 @@ document.getElementById("open-password").addEventListener("change", function() {
 		document.getElementById("popap-input").setAttribute("type", "text");
 	} else {
 		document.getElementById("popap-input").setAttribute("type", "password");
+	}
+});
+
+///////////////// POPAP__SING-IN /////////////////
+document.getElementById("open-password1").addEventListener("change", function() {
+
+	document.getElementById("popap-icon1").classList.toggle("password__open");
+
+	if (document.getElementById("open-password1").checked) {
+		document.getElementById("popap-input1").setAttribute("type", "text");
+	} else {
+		document.getElementById("popap-input1").setAttribute("type", "password");
+	}
+});
+
+///////////////// POPAP__SING-UP /////////////////
+document.getElementById("open-password2").addEventListener("change", function() {
+
+	document.getElementById("popap-icon2").classList.toggle("password__open");
+
+	if (document.getElementById("open-password2").checked) {
+		document.getElementById("popap-input2").setAttribute("type", "text");
+	} else {
+		document.getElementById("popap-input2").setAttribute("type", "password");
+	}
+});
+
+document.getElementById("open-password3").addEventListener("change", function() {
+
+	document.getElementById("popap-icon3").classList.toggle("password__open");
+
+	if (document.getElementById("open-password3").checked) {
+		document.getElementById("popap-input3").setAttribute("type", "text");
+	} else {
+		document.getElementById("popap-input3").setAttribute("type", "password");
+	}
+});
+
+
+document.getElementById("open-password4").addEventListener("change", function() {
+
+	document.getElementById("popap-icon4").classList.toggle("password__open");
+
+	if (document.getElementById("open-password4").checked) {
+		document.getElementById("popap-input4").setAttribute("type", "text");
+	} else {
+		document.getElementById("popap-input4").setAttribute("type", "password");
 	}
 });
 
