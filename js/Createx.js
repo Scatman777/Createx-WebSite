9cb90b58145b9971004b94daf92184d78641d07f
hostr777 <divakovbogdan11@gmail.com>
2023-11-07 20:01:55 +0300

    Update

--- js/Createx.js
+++ js/Createx.js
@@ -102,16 +102,15 @@ function burgerLangList() {
 ///////////////// ALL-SECTION__BORDER-GREEN /////////////////
 const sectionButtons = document.querySelector('.function-borderColumn');
 sectionButtons.addEventListener("click", function (e) {
-		let borderGreen = e.target;
-		if (borderGreen.closest('.function-borderItem')) {
-			borderGreen.closest('.function-borderItem').classList.toggle('border-green');
-		}
-	});
-
-sectionButtons.addEventListener("click", function (e) {
 	let borderGreen = e.target;
 	if (borderGreen.closest('.function-borderItem')) {
 		borderGreen.closest('.function-borderItem').classList.toggle('border-green');
+	}
+});
+sectionButtons.addEventListener("click", function (e) {
+	let backgroundGreen = e.target;
+	if (backgroundGreen.closest('.function-backgroundItem')) {
+		backgroundGreen.closest('.function-backgroundItem').classList.toggle('background-green');
 	}
 });
 
