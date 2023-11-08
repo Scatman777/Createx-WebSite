e55d596a4e8e234800f6ce59985bba6ee93b8988
hostr777 <divakovbogdan11@gmail.com>
2023-11-08 09:19:08 +0300

    Update

--- js/Createx.js
+++ js/Createx.js
@@ -19,74 +19,53 @@ for (let elm of elements) {
 }
 
 ///////////////// POPAP__SING-IN /////////////////
-
-// const openPassword = document.getElementById('password');
-// openPassword.addEventListener("click", function (e) {
-// 	let targetItemOpen = e.target
-// 	// if (targetItemOpen.closest("open-password1").checked) {
-// 	// 	targetItemOpen.closest("popap-input1").setAttribute("type", "text");
-// 	// }
-// 	if (targetItemOpen.closest('.password-control')) {
-// 		targetItemOpen.closest('.block-input__input').setAttribute("type", "text")
-// 	} else {
-// 		document.getElementById("popap-input1").setAttribute("type", "password");
-// 	}
-// 	document.getElementById("popap-icon1").classList.toggle("password__open");
-// });
-
-// document.getElementById("open-password1").addEventListener("change", function() {
-
-// 	document.getElementById("popap-icon1").classList.toggle("password__open");
-
-// 	if (document.getElementById("open-password1").checked) {
-// 		document.getElementById("popap-input1").setAttribute("type", "text");
-// 	} else {
-// 		document.getElementById("popap-input1").setAttribute("type", "password");
-// 	}
-// });
+document.getElementById("open-password1").addEventListener("change", function() {
+	document.getElementById("popap-icon1").classList.toggle("password__open");
+	if (document.getElementById("open-password1").checked) {
+		document.getElementById("popap-input1").setAttribute("type", "text");
+	} else {
+		document.getElementById("popap-input1").setAttribute("type", "password");
+	}
+});
 
 ///////////////// POPAP__SING-UP /////////////////
-// document.getElementById("open-password2.1").addEventListener("change", function() {
-
-// 	document.getElementById("popap-icon2.1").classList.toggle("password__open");
-
-// 	if (document.getElementById("open-password2.1").checked) {
-// 		document.getElementById("popap-input2.1").setAttribute("type", "text");
-// 	} else {
-// 		document.getElementById("popap-input2.1").setAttribute("type", "password");
-// 	}
-// });
-// document.getElementById("open-password2.2").addEventListener("change", function() {
-
-// 	document.getElementById("popap-icon2.2").classList.toggle("password__open");
-
-// 	if (document.getElementById("open-password2.2").checked) {
-// 		document.getElementById("popap-input2.2").setAttribute("type", "text");
-// 	} else {
-// 		document.getElementById("popap-input2.2").setAttribute("type", "password");
-// 	}
-// });
+document.getElementById("open-password2.1").addEventListener("change", function() {
+	document.getElementById("popap-icon2.1").classList.toggle("password__open");
+	if (document.getElementById("open-password2.1").checked) {
+		document.getElementById("popap-input2.1").setAttribute("type", "text");
+	} else {
+		document.getElementById("popap-input2.1").setAttribute("type", "password");
+	}
+});
+document.getElementById("open-password2.2").addEventListener("change", function() {
+	document.getElementById("popap-icon2.2").classList.toggle("password__open");
+	if (document.getElementById("open-password2.2").checked) {
+		document.getElementById("popap-input2.2").setAttribute("type", "text");
+	} else {
+		document.getElementById("popap-input2.2").setAttribute("type", "password");
+	}
+});
 
 ///////////////// SECTION-PROFILE__FORM /////////////////
-// document.getElementById("open-password3").addEventListener("change", function() {
-// 	document.getElementById("popap-icon3").classList.toggle("password__open");
+document.getElementById("open-password3").addEventListener("change", function() {
+	document.getElementById("popap-icon3").classList.toggle("password__open");
 
-// 	if (document.getElementById("open-password3").checked) {
-// 		document.getElementById("popap-input3").setAttribute("type", "text");
-// 	} else {
-// 		document.getElementById("popap-input3").setAttribute("type", "password");
-// 	}
-// });
+	if (document.getElementById("open-password3").checked) {
+		document.getElementById("popap-input3").setAttribute("type", "text");
+	} else {
+		document.getElementById("popap-input3").setAttribute("type", "password");
+	}
+});
 
-// document.getElementById("open-password4").addEventListener("change", function() {
-// 	document.getElementById("popap-icon4").classList.toggle("password__open");
+document.getElementById("open-password4").addEventListener("change", function() {
+	document.getElementById("popap-icon4").classList.toggle("password__open");
 
-// 	if (document.getElementById("open-password4").checked) {
-// 		document.getElementById("popap-input4").setAttribute("type", "text");
-// 	} else {
-// 		document.getElementById("popap-input4").setAttribute("type", "password");
-// 	}
-// });
+	if (document.getElementById("open-password4").checked) {
+		document.getElementById("popap-input4").setAttribute("type", "text");
+	} else {
+		document.getElementById("popap-input4").setAttribute("type", "password");
+	}
+});
 
 ///////////////// HEADER__MENU-LANG /////////////////
 function MenuLangList() {
@@ -113,20 +92,6 @@ sectionButtons.addEventListener("click", function (e) {
 		backgroundGreen.closest('.function-backgroundItem').classList.toggle('background-green');
 	}
 });
-
-///////////////// SECTION-SUBSCRIBE__BORDER /////////////////
-// function buttonFunctionGender1() {
-//     document.getElementById("buttonGender1").classList.toggle("background-green");
-// }
-// function buttonFunctionGender2() {
-//     document.getElementById("buttonGender2").classList.toggle("background-green");
-// }
-// function buttonFunctionGender3() {
-//     document.getElementById("buttonGender3").classList.toggle("background-green");
-// }
-// function buttonFunctionGender4() {
-//     document.getElementById("buttonGender4").classList.toggle("background-green");
-// }
 
 ///////////////// SECTION-CATALOG /////////////////
 function HideFilters() {
