fec806385a6146c7e872de840e97a6b9ef217871
hostr777 <divakovbogdan11@gmail.com>
2023-11-22 22:05:37 +0300

    Update

--- js/Createx-deg.js
+++ js/Createx-deg.js
@@ -61,38 +61,3 @@ sectionButtons.addEventListener("click", function (e) {
 		backgroundGreen.closest('.function-backgroundItem').classList.toggle('background-green');
 	}
 });
-
-///////////////// SECTION-PRODCT__SWIEPR /////////////////
-const swiperProduct = new Swiper('.swiper', {
-	watchOverflow: false,
-	slidesPerView: 'auto',
-	slidesPerGroup: 1,
-	slidesPerColumn: 1,
-
-	pagination: {
-	  el: '.swiper-pagination',
-	  clickable: true,
-	},
-
-	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
-	},
-});
-
-const swiperCloth = new Swiper('.swiper-cloth', {
-	watchOverflow: false,
-	slidesPerView: 'auto',
-	slidesPerGroup: 1,
-	slidesPerColumn: 1,
-
-	pagination: {
-	  el: '.swiper-cloth-pagination',
-	  clickable: true,
-	},
-
-	navigation: {
-	  nextEl: '.swiper-cloth__next',
-	  prevEl: '.swiper-cloth__prev',
-	},
-});
